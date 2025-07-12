35b498e0cc897be1f96f7d9925290ef1d2aa59ce
SamzelMable <samzelmable@gmail.com>
2025-07-12 10:56:18 +1000

    Add Redux store and fetch posts from Reddit

---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import postsReducer from '../features/posts/PostsSlice';
+
+export const store = configureStore({
+  reducer: {
+    posts: postsReducer,
+  },
+});
